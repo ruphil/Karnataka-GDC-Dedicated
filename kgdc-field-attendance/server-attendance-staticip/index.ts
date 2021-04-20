72440ece80dfb7bf6f1078984004ce917cdbff14
ruphil <rukmangadanies@gmail.com>
2021-04-20 22:27:07 +0530

    p

--- kgdc-field-attendance/server-attendance-staticip/index.ts
+++ kgdc-field-attendance/server-attendance-staticip/index.ts
@@ -3,33 +3,22 @@ import { join } from 'path';
 import http from 'http'
 import express from 'express';
 
-import { checkAdmin } from './handlesockets';
-
-const app: express.Application = express();
+import { handleWebSocketConnection } from './handlesockets';
 
 const staticDir: string = join(__dirname, 'frontend');
-
+const app: express.Application = express();
 app.use('/', express.static(staticDir));
-
 app.get('/', function(req, res) {
     res.sendFile(join(staticDir, 'index.html'));
 });
 
 const server: http.Server = http.createServer(app);
 const wss: Server = new Server({ server });
-
 wss.on('connection', (ws: WebSocket) => {
-    ws.on('message', (data: WebSocket.Data)=>{
-        let msgObj = JSON.parse(Buffer.from(data.toString(), 'base64').toString());
-        console.log(msgObj);
-
-        if (msgObj.requesttype ==)
-        checkAdmin(ws, msgObj)
-    });
+    handleWebSocketConnection(ws);
 });
 
-const port = 3010;
-
-server.listen(port, ()=> {
-    console.log(`Server listening on port ${port}`);
+const PORT = 3010;
+server.listen(PORT, ()=> {
+    console.log(`Server listening on port ${PORT}`);
 });
