30c15f575df78066dbf4005f1f13547e5cfb628c
ruphil <rukmangadanies@gmail.com>
2021-04-20 18:21:16 +0530

    p

--- kgdc-dsp-monitor/dsp-monitor-frontend/src/store/index.js
+++ kgdc-dsp-monitor/dsp-monitor-frontend/src/store/index.js
@@ -2,8 +2,16 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    // wsurl: 'ws://59.88.201.244:4010',
+    wsurl: 'ws://localhost:4010',
+  },
+  getters: {
+    getWSURL: state => {
+      return state.wsurl;
+    },
   },
   mutations: {
+
   },
   actions: {
   },
