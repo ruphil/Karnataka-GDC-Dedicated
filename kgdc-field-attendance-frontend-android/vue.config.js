0b6695bb7e71cec505daaa96e02757c36a4b5e36
ruphil <rukmangadanies@gmail.com>
2021-04-22 23:48:25 +0530

    Update vue.config.js

--- kgdc-field-attendance-frontend-android/vue.config.js
+++ kgdc-field-attendance-frontend-android/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    // publicPath: process.env.NODE_ENV === 'production'
+    // ? '/attendance-register-android-host/'
+    // : '/',
     productionSourceMap: false,
     css: {
         extract: false
