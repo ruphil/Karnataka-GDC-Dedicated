e9c1fa2c7342cd038a031994b9c619d170ab9ebb
ruphil <rukmangadanies@gmail.com>
2021-04-24 10:22:21 +0530

    p

--- kgdc-field-attendence-socket-server/src/index.ts
+++ kgdc-field-attendence-socket-server/src/index.ts
@@ -1,16 +1,12 @@
 import WebSocket, { Server } from 'ws';
 import http from 'http'
 import express from 'express';
-import { join } from 'path';
 
 import { handleWebSocketConnection } from './handlersocket';
 
-const staticDir: string = join(__dirname, 'frontend-admin');
-
 const app: express.Application = express();
-app.use(express.static(staticDir));
 app.get('*', function(req, res) {
-    res.sendFile(join(staticDir, 'index.html'));
+    res.send('Message from HTTP Server: Attendance Socket Server Running Fine!');
 });
 
 const server: http.Server = http.createServer(app);
