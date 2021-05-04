96a68f98bff994a70ff1c5522f1a40cea102d915
ruphil <rukmangadanies@gmail.com>
2021-05-05 00:24:18 +0530

    Update featureUploader.ts

--- field-gis-openlayers-vue-frontend/src/composables/featureUploader.ts
+++ field-gis-openlayers-vue-frontend/src/composables/featureUploader.ts
@@ -47,7 +47,6 @@ const featureUploader = () => {
 
             axios({
                 method: 'POST',
-                // url: 'http://localhost:8080/geoserver/kgdc/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=kgdc:flightlines',
                 url: 'http://localhost:8080/geoserver/ows?service=wfs&version=2.0.0&request=GetCapabilities',
                 headers: {
                     // 'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
