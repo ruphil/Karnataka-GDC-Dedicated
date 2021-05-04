9e884739f6c1071f6ec20f4f2a8da08e9ba58caa
ruphil <rukmangadanies@gmail.com>
2021-05-04 08:48:05 +0530

    p

--- field-gis-openlayers-vue-frontend/src/components/FlightsManager.vue
+++ field-gis-openlayers-vue-frontend/src/components/FlightsManager.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
-        <span>Add Flightline KML:</span><br/>
-        <input type="file" />
+        <div id="fileslistholder">
+            <div id="fileslist">
+                J
+            </div>
+            <div id="filesaddbtnscontainer">
+                <input id="flightlinekml" type="file" style="display:none;" />
+                <input id="shapefiles" type="file" style="display:none;" />
+                <span>
+                    <button class="olbtns" id="kmladdbtn" onclick="document.getElementById('flightlinekml').click();">+ KML</button>
+                    <button class="olbtns" id="shapesaddbtn" onclick="document.getElementById('shapefiles').click();">+ Shape</button>
+                </span>
+            </div>
+        </div>
+        <!-- <span>Add Flightline KML:</span><br/>
+        <input type="file" /> -->
     </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
