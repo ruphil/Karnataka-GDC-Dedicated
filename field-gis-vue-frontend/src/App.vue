246ef726f8278a6ae1b64738c877f7d36b0506f3
ruphil <rukmangadanies@gmail.com>
2021-05-09 11:39:58 +0530

    p

--- field-gis-vue-frontend/src/App.vue
+++ field-gis-vue-frontend/src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <div id="app">
+  <div class="app">
     <NavBar />
     <LeftSideBar />
     <MapContainer />
     <ControlsContainer />
+    <div class="latlon"></div>
+    <div class="globaltoast"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import NavBar from './components/Navbar.vue';
+import './App.scss';
+
+import NavBar from './components/NavBar.vue';
 import LeftSideBar from './components/LeftSideBar.vue';
 import MapContainer from './components/MapContainer.vue';
 import ControlsContainer from './components/ControlsContainer.vue';
-
-import './styles/NavBar.scss';
 
 export default defineComponent({
   components: {
--- field-gis-vue-frontend/src/components/Navbar.vue
+++ field-gis-vue-frontend/src/components/Navbar.vue
@@ -1,7 +1,29 @@
 <template>
-  <div id="navbarcontainer">
-    <div>
-      jk
+  <div class="navbarcontainer">
+    <div class="navbar">
+      <span class="logo">
+        <img src="../assets/logo.png" width="20"/>
+      </span>
+      <span class="user">
+        <button class="loginbtn">Login</button>
+      </span>
+      <span class="title">Karnataka Geospatial Data Centre</span>
+    </div>
+
+    <div class="loginbox">
+      
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+import './NavBar.scss';
+
+export default defineComponent({
+  setup() {
+    const username = ref('');
+  },
+})
+</script>
