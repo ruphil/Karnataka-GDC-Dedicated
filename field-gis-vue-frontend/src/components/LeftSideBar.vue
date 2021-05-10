459f60d291c108a703250923caa7c1a7087bf5de
ruphil <rukmangadanies@gmail.com>
2021-05-10 14:09:15 +0530

    p

--- field-gis-vue-frontend/src/components/LeftSideBar.vue
+++ field-gis-vue-frontend/src/components/LeftSideBar.vue
@@ -1,20 +1,53 @@
 <template>
-    <div id="leftsidebar">
-        <div class="expandbtncontainer">
-            <button class="expandbtn">></button>
+    <div id="leftsidebar" ref="leftsidebar">
+        <div class="soilogo">
+            <span class="logo"></span>
+            <span class="soi"></span>
         </div>
-        
+        <div class="categoryitems">
+            <div class="category">
+                <span class="icon"><span class="material-icons-outlined">add_location</span></span>
+                <span class="label" v-show="expanded">Add Marked Villages</span>
+            </div>
+            <div class="category">
+                <span class="icon"><span class="material-icons-outlined">highlight_alt</span></span>
+                <span class="label" v-show="expanded">Make Mission Plan</span>
+            </div>
+            <div class="category">
+                <span class="icon"><span class="material-icons-outlined">flight_takeoff</span></span>
+                <span class="label" v-show="expanded">Flights Manager</span>
+            </div>
+        </div>
+        <div class="expandbtncontainer">
+            <button class="expandbtn" v-show="!expanded" v-on:click="toggleExpansion"><span class="material-icons-outlined">arrow_forward</span></button>
+            <button class="expandbtn" v-show="expanded" v-on:click="toggleExpansion"><span class="material-icons-outlined">arrow_back</span></button>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 import './LeftSideBar.scss';
 
 export default defineComponent({
     setup() {
+        const leftsidebar = ref();
+        const expanded = ref(false);
         
+        const toggleExpansion = () => {
+            console.log(leftsidebar.value);
+
+            // if(expanded.value){
+            //     leftsidebar.value.style.width = 'auto';
+            // } else {
+            //     leftsidebar.value.style.width = '5vw';
+            // }
+
+            expanded.value = !expanded.value;
+        }
+        
+        return { leftsidebar, expanded, toggleExpansion }
     },
 })
 </script>
