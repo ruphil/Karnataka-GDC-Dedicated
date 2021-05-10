90cf159de442b5092db9f5d2a70279a1005c6a08
ruphil <rukmangadanies@gmail.com>
2021-05-10 22:23:20 +0530

    p

--- field-gis-vue-frontend/src/controllerviews/MarkedVillages.vue
+++ field-gis-vue-frontend/src/controllerviews/MarkedVillages.vue
@@ -1,19 +1,25 @@
 <template>
     <div id="markedvillages">
         <div class="toolcontainer">
-            <button class="toolbox"><span class="material-icons-outlined">handyman</span></button>
+            <button class="togglebtn" v-on:click="showToolBox = !showToolBox"><span class="material-icons-outlined">handyman</span></button>
+            <div class="toolbox" v-show="showToolBox">
+                <span class="material-icons-outlined tool">file_upload</span>
+                <span class="material-icons-outlined tool">file_download</span>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 import './MarkedVillages.scss';
 
 export default defineComponent({
     setup() {
-        
+        const showToolBox = ref(false);
+
+        return { showToolBox }
     },
 })
 </script>
