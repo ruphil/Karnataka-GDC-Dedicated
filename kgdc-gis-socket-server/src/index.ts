2e217a5e55e382b658d7a897ecd05e8c721b8b7d
ruphil <rukmangadanies@gmail.com>
2021-05-23 23:24:18 +0530

    p

--- kgdc-gis-socket-server/src/index.ts
+++ kgdc-gis-socket-server/src/index.ts
@@ -4,28 +4,28 @@ import { handlews } from './wshandle';
 import { checkuser } from './authenticator';
 
 const server = http.createServer();
-const wss = new Server({ noServer: true });
+const geojsonwsserver = new Server({ noServer: true });
 
-wss.on('connection', (ws: WebSocket, request: any) => {
+geojsonwsserver.on('connection', (ws: WebSocket, request: any) => {
     console.log('came here 1');
     handlews(ws, request);
 });
 
 server.on('upgrade', (request, socket, head) => {
     const reqUrl = new URL(request.url, request.headers.origin);
-    // console.log(reqUrl);
-    // console.log(reqUrl.searchParams);
-    const { validuser, roles } = checkuser(reqUrl.searchParams);
+    const searchParams = reqUrl.searchParams;
+    console.log(searchParams, reqUrl);
+
+    const { validuser, roles } = checkuser(searchParams);
     console.log(validuser, roles);
-    
+
     if(validuser){
-        console.log('came here 2');
-        wss.handleUpgrade(request, socket, head, function done(ws) {
-            console.log('came here 3');
-            wss.emit('connection', ws, request);
-        });
+        switch (reqUrl.pathname){
+            case '/getgeojsons': 
+                geojsonwsserver.handleUpgrade(request, socket, head, (ws: WebSocket) => { geojsonwsserver.emit('connection', ws, request); });
+                break;
+        }
     } else {
-        console.log('came here 12');
         socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
         socket.destroy();
         return;
--- kgdc-gis-socket-server/src/wshandle.ts
+++ kgdc-gis-socket-server/src/wshandle.ts
@@ -2,8 +2,6 @@ import WebSocket from 'ws';
 
 export const handlews = (ws: WebSocket, request: any) => {
     const reqUrl = new URL(request.url, request.headers.origin);
-    console.log(reqUrl);
-    console.log(reqUrl.searchParams);
 
     console.log('came here 4');
     ws.send('jack');
