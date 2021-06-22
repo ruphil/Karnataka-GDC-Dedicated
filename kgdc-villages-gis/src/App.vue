e9d1e5d0bd73a9c0a91168bd2d55a8f8944602d5
ruphil <rukmangadanies@gmail.com>
2021-06-22 11:44:52 +0530

    p

--- kgdc-villages-gis/src/App.vue
+++ kgdc-villages-gis/src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <NavBar />
+  <MapContainer />
+  <ControlsContainer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import NavBar from './components/NavBar.vue';
+import MapContainer from './components/NavBar.vue';
+import ControlsContainer from './components/NavBar.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    NavBar, MapContainer, ControlsContainer
   }
 });
 </script>
