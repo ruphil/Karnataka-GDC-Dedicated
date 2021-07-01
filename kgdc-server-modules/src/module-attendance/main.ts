8b902a0786fede0e4c1a4e68f5483ea9208319b5
ruphil <rukmangadanies@gmail.com>
2021-07-01 14:32:03 +0530

    p

--- kgdc-server-modules/src/module-attendance/main.ts
+++ kgdc-server-modules/src/module-attendance/main.ts
@@ -1,1 +1,11 @@
-console.log('attendance');
+import WebSocket, { Server } from 'ws';
+import { handleWebSocketConnection } from './sockethandler';
+
+const PORT = 3050;
+const wss = new Server({ port: PORT });
+
+wss.on('connection', (ws: WebSocket, roles: any) => {
+    handleWebSocketConnection(ws);
+});
+
+console.log(`Server: Attendance Module may have started on Port:${PORT}`);
