59bde8f7e175e7a5c9062e27ecf2a64ce50c396e
ruphil <rukmangadanies@gmail.com>
2021-07-08 19:57:10 +0530

    p

---
+++ kgdc-server-modules/src/module-fileslist-serveapprove/getfilelist.ts
@@ -0,0 +1,27 @@
+import WebSocket from 'ws';
+import { Client } from 'pg';
+
+const connectionString = 'postgres://postgres:kgdcgis@localhost:5432/kgdcdb';
+
+export const getFileList = (ws: WebSocket, msgObj: any) => {
+    const { uniquevillagecode } = msgObj;
+    let getQuery = `SELECT * FROM abadilimits WHERE UNIQUEVILLAGECODE='${uniquevillagecode}'`;
+
+    const client = new Client({ connectionString });
+    client.connect();
+
+    client.query(getQuery)
+    .then((res) => {
+        let abadilist = res.rows;
+        let responseObj = { response: 'getfilelist', requestStatus: 'success', validUser: true, abadilist };
+        ws.send(Buffer.from(JSON.stringify(responseObj)).toString('base64'));
+    })
+    .catch((err) => {
+        console.log(err);
+        let responseObj = { response: 'getfilelist', requestStatus: 'failure', validUser: true, status: 'Error in SQL' };
+        ws.send(Buffer.from(JSON.stringify(responseObj)).toString('base64'));
+    })
+    .finally(() => {
+        client.end();
+    });
+}
--- kgdc-server-modules/src/module-fileslist-serveapprove/sockethandler.ts
+++ kgdc-server-modules/src/module-fileslist-serveapprove/sockethandler.ts
@@ -1,15 +1,25 @@
 import WebSocket from 'ws';
 
+import { checkValidUserNGetRoles } from '../common-ts/userRolesAdminCheck';
+import { getFileList } from './getfilelist';
 
 export const handleWebSocketConnection = (ws: WebSocket) => {
     ws.on('message', (data: WebSocket.Data)=>{
         let msgObj = JSON.parse(Buffer.from(data.toString(), 'base64').toString());
         console.log(msgObj);
 
-        if(msgObj.request == 'getfilelist'){
-            
-        } else {
-            ws.send('invalidrequest');
-        }
+        checkValidUserNGetRoles(msgObj)
+        .then(() => {
+            if(msgObj.request == 'getfilelist'){
+                getFileList(ws, msgObj);
+            } else {
+                let responseObj = { response: 'getfilelist', requestStatus: 'failure', validUser: true, status: 'Invalid Request' };
+                ws.send(Buffer.from(JSON.stringify(responseObj)).toString('base64'));
+            }
+        })
+        .catch(() => {
+            let responseObj = { response: 'getfilelist', requestStatus: 'failure', validUser: false, status: 'Unauthorized Request' };
+            ws.send(Buffer.from(JSON.stringify(responseObj)).toString('base64'));
+        })
     });
 }
